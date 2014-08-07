ddf8e9fa693714df162ec50877733c975ee09b4b
Zack Fisch <zack@zfisch.com>
2014-08-07 16:54:48 -0400

    Dynamic testing for title of pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let (:base_title) { "Fluttr" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
     	it 'should have the right title' do
   		visit '/static_pages/home' 
-  		expect(page).to have_title('Fluttr | Home')
+  		expect(page).to have_title(" #{base_title} | Home")
   	end
   end
 
@@ -24,7 +26,7 @@ describe "Static pages" do
 
     	it 'should have the right title' do
   		visit '/static_pages/help' 
-  		expect(page).to have_title('Fluttr | Help')
+  		expect(page).to have_title(" #{base_title} | Help")
   	end
   end
 
@@ -36,7 +38,7 @@ describe "Static pages" do
 
   	it 'should have the right title' do
   		visit '/static_pages/about' 
-  		expect(page).to have_title('Fluttr | About')
+  		expect(page).to have_title(" #{base_title} | About")
   end
 end
 
@@ -50,7 +52,7 @@ end
 	describe 'Contact Page' do
 		it 'should have the right title' do
 			visit '/static_pages/contact'
-			expect(page).to have_title('Fluttr | Contact Us')
+			expect(page).to have_title(" #{base_title} | Contact Us")
 		end
 	end
 
