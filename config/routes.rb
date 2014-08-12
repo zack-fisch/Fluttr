06cf3f6199a031b1f5902d709aa893296e1eb6ea
Zack Fisch <zack@zfisch.com>
2014-08-12 14:20:36 -0400

    Adding show template for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
 
-  get "users/new"
+  resources :users
   root "static_pages#home"
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
