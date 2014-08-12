d72e088f5bc6462ea31422d31d4c15b31671241e
Zack Fisch <zack@zfisch.com>
2014-08-12 13:54:55 -0400

    Adding BCrypt password to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,17 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before { @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar") }
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
 
   describe "when name is not present" do
     before { @user.name = " " }
