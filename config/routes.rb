57794a7e6fad839754c05630f367251143c94d44
Zack Fisch <zack@zfisch.com>
2014-08-13 20:27:45 -0400

    Creating form for login

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ SampleApp::Application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
-  get 'signin', to: 'sessions#new'
+  get '/signin', to: 'sessions#new'
   get '/signout', to: 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
