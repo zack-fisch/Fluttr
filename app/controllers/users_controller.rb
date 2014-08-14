56543c0c9b482fef9c478606722fa971f2763149
Zack Fisch <zack@zfisch.com>
2014-08-14 17:03:43 -0400

    Adding update action for editing user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,10 @@ class UsersController < ApplicationController
 	  end
 	end
 
+  def update
+    @user = User.find(params[:id])
+  end
+
   private
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
