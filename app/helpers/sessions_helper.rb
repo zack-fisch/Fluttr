b018f431e8339a9b5e93835bcdf204ca1a1edaef
Zack Fisch <zack@zfisch.com>
2014-08-13 20:57:41 -0400

    Creating sign in helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,8 @@
 module SessionsHelper
+	def sign_in(user)
+    remember_token = User.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.digest(remember_token))
+    self.current_user = user
+  end
 end
