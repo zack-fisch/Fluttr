276e571eb73f58b9683e50d2fe7041130e0241ac
Zack Fisch <zack@zfisch.com>
2014-08-13 20:20:03 -0400

    Adding session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root "static_pages#home"
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
