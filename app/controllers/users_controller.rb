1077936c452fec20ec81bc7ded73238ded3afeaf
Zack Fisch <zack@zfisch.com>
2014-08-14 18:10:38 -0400

    Requiring signin before

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+
   def new
   	@user = User.new
   end
@@ -35,5 +37,11 @@ class UsersController < ApplicationController
   private
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # before filters
+
+  def signed_in_user
+    redirect_to signin_url, notice: 'Please sign in.' unless signed_in?
   end
 end
