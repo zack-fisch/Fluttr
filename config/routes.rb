8f029cdf17f1f22fff360e5d1a68c242e819e013
Zack Fisch <zack@zfisch.com>
2014-08-20 17:22:23 -0400

    Adding microposts controller, ability to create them

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   root "static_pages#home"
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
